a5d95744001d8c578bc306c6a2a1f9e2eda13958
vku400 <vinod.kumar@cambiumnetworks.com>
2025-04-29 20:35:06 +0530

    array rotation added

---
+++ Data_Structures/array_rotation.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    rotated_arr = [None for _ in arr]
+    size = len(arr)
+    for index, value in enumerate(arr):
+        r = ((size-d)+index)
+        print("value", value)
+        print("previous index", index)
+        print("index", r%size)
+        rotated_arr[r%size] = value
+    return rotated_arr
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
